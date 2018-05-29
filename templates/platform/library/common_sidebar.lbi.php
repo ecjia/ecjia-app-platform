a8cd71177a7ca6efae9b287ef71f2005f9168f61
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-05-29 15:28:56 +0800

    提交platform文件修改

---
+++ templates/platform/library/common_sidebar.lbi.php
@@ -0,0 +1,23 @@
+<?php defined('IN_ECJIA') or exit('No permission resources.');?>
+        <div class="main-menu menu-static menu-light menu-accordion menu-shadow" data-scroll-to-active="true">
+          <div class="main-menu-content">
+            <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+              <li class=" nav-item"><a href="index.html"><i class="icon-home"></i><span class="menu-title" data-i18n="nav.dash.main">Dashboard</span><span class="badge badge badge-info badge-pill float-right mr-2">5</span></a>
+                <ul class="menu-content">
+                  <li><a class="menu-item" href="dashboard-analytics.html" data-i18n="nav.dash.analytics">Analytics</a>
+                  </li>
+                  <li><a class="menu-item" href="dashboard-project.html" data-i18n="nav.dash.project">Project</a>
+                  </li>
+                  <li><a class="menu-item" href="dashboard-ecommerce.html" data-i18n="nav.dash.ecommerce">eCommerce</a>
+                  </li>
+                  <li><a class="menu-item" href="dashboard-crm.html" data-i18n="nav.dash.crm">CRM</a>
+                  </li>
+                  <li><a class="menu-item" href="dashboard-fitness.html" data-i18n="nav.dash.fitness">Fitness</a>
+                  </li>
+                </ul>
+              </li>
+              <li class=" navigation-header"><span data-i18n="nav.category.layouts">Layouts</span><i class="ft-more-horizontal ft-minus" data-toggle="tooltip" data-placement="right" data-original-title="Layouts"></i>
+              </li>
+            </ul>
+          </div>
+        </div>
