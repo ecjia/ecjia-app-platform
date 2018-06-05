7907055a3a8fbaf4f894f65d2c819e745ca4ec8d
东方皓 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-06-05 16:47:12 +0800

    优化代码格式

--- dashboard.php
+++ dashboard.php
@@ -46,7 +46,8 @@
 //
 defined('IN_ECJIA') or exit('No permission resources.');
 
-class dashboard extends ecjia_platform {
+class dashboard extends ecjia_platform 
+{
 	
     public function init() {
         
