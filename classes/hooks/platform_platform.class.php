04587400eb7a842beefcfbd59e7dba0132f41454
东方皓 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-06-05 18:20:07 +0800

    修改公众平台可用应用

--- classes/hooks/platform_platform.class.php
+++ classes/hooks/platform_platform.class.php
@@ -46,23 +46,23 @@
 //
 defined('IN_ECJIA') or exit('No permission resources.');
 
-class merchant_merchant_hooks {
+class platform_platform_hooks {
 
     public static function ecjia_builtin_app_bundles($apps) {
-        $merchant_apps = RC_Config::get('merchant.apps');
+        $merchant_apps = RC_Config::get('platform.apps');
         return $merchant_apps;
     }
     
     
-    public static function ecjia_merchant_dashboard_contact() {
+    public static function ecjia_platform_dashboard_contact() {
         ecjia_merchant::$controller->display(
-		    RC_Package::package('app::merchant')->loadTemplate('merchant/library/widget_merchant_dashboard_contact.lbi', true)
+		    RC_Package::package('app::platform')->loadTemplate('merchant/library/widget_platform_dashboard_contact.lbi', true)
 		);
     }
 
 }
 
-RC_Hook::add_filter( 'ecjia_builtin_app_bundles', array('merchant_merchant_hooks', 'ecjia_builtin_app_bundles') );
-RC_Hook::add_filter( 'merchant_dashboard_right4', array('merchant_merchant_hooks', 'ecjia_merchant_dashboard_contact'), 2 );
+RC_Hook::add_filter( 'ecjia_builtin_app_bundles', array('platform_platform_hooks', 'ecjia_builtin_app_bundles') );
+RC_Hook::add_filter( 'platform_dashboard_right4', array('platform_platform_hooks', 'ecjia_platform_dashboard_contact'), 2 );
 
 // end
