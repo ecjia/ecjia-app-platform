6f81f7a07d26c274db1283832001981c11db15cc
东方皓 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-06-07 13:35:19 +0800

    更换父类

--- classes/Frameworks/Users/AdminUserAllotPurview.php
+++ classes/Frameworks/Users/AdminUserAllotPurview.php
@@ -2,14 +2,14 @@
 
 namespace Ecjia\App\Platform\Frameworks\Users;
 
-use Ecjia\App\Platform\Frameworks\Component\AllotPurview;
+use Ecjia\System\Frameworks\Meta\MetaAbstract;
 
-class AdminUserAllotPurview extends AllotPurview
+class AdminUserAllotPurview extends MetaAbstract
 {
     
-    protected $meta_key = 'platform_allot_purview';
+    protected $meta_key     = 'platform_allot_purview';
     
-    protected $object_type = 'ecjia.system';
+    protected $object_type  = 'ecjia.system';
     
     protected $object_group = 'admin_user';
     
