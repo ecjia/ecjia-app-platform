251f3d73b3e2468ff83dc1fa99c1b27361a4ac1d
东方皓 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-06-19 15:04:55 +0800

    修改空链接

--- templates/platform/library/common_header.lbi.php
+++ templates/platform/library/common_header.lbi.php
@@ -3,7 +3,7 @@
 	<div class="navbar-wrapper">
 		<div class="navbar-header">
       		<ul class="nav navbar-nav flex-row">
-        		<li class="nav-item mobile-menu d-md-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="icons-simple-line-icons.html#"><i class="ft-menu font-large-1"></i></a></li>
+        		<li class="nav-item mobile-menu d-md-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ft-menu font-large-1"></i></a></li>
         		<li class="nav-item">
         			<a class="navbar-brand" href="index.html">
         			<img class="brand-logo" alt="robust admin logo" src="../../../content/apps/platform/statics/platform/images/logo/logo-dark-sm.png">
@@ -16,11 +16,11 @@
     	<div class="navbar-container content">
       		<div class="collapse navbar-collapse" id="navbar-mobile">
         		<ul class="nav navbar-nav mr-auto float-left">
-          			<li class="nav-item d-none d-md-block"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="icons-simple-line-icons.html#"><i class="ft-menu">         </i></a></li>
+          			<li class="nav-item d-none d-md-block"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ft-menu">         </i></a></li>
                     <!-- {include file="library/common_header_mega.lbi.php"} -->
           			<li class="nav-item d-none d-md-block"><a class="nav-link nav-link-expand" href="#"><i class="ficon ft-maximize"></i></a></li>
           			<li class="nav-item nav-search">
-          				<a class="nav-link nav-link-search" href="icons-simple-line-icons.html#"><i class="ficon ft-search"></i></a>
+          				<a class="nav-link nav-link-search" href="#"><i class="ficon ft-search"></i></a>
                         <div class="search-input">
                           	<input class="input" type="text" placeholder="Explore Robust...">
                         </div>
@@ -28,7 +28,7 @@
         		</ul>
         		<ul class="nav navbar-nav float-right">         
           			<li class="dropdown dropdown-language nav-item">
-          				<a class="dropdown-toggle nav-link" id="dropdown-flag" href="icons-simple-line-icons.html#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="flag-icon flag-icon-cn"></i><span>简体中文</span><span class="selected-language"></span></a>
+          				<a class="dropdown-toggle nav-link" id="dropdown-flag" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="flag-icon flag-icon-cn"></i><span>简体中文</span><span class="selected-language"></span></a>
                         <div class="dropdown-menu" aria-labelledby="dropdown-flag">
                         	<a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="flag-icon flag-icon-cn"></i> 简体中文</a>
                         </div>
@@ -36,7 +36,7 @@
                     <!-- {include file="library/common_header_notifications.lbi.php"} -->
                     <!-- {include file="library/common_header_messages.lbi.php"} -->
           			<li class="dropdown dropdown-user nav-item">
-          				<a class="dropdown-toggle nav-link dropdown-user-link" href="{$current_user->getProfileSettingUrl()}#" data-toggle="dropdown">
+          				<a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
           					<span class="avatar avatar-online"><img src="{$current_user->getAvatarUrl()}" alt="{{$current_user->getUserName()}}"><i></i></span>
           					<span class="user-name">{$current_user->getUserName()}</span>
           				</a>
