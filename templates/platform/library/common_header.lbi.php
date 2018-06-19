59c23556cbf5aebcda949664da0e4d5eb5362868
东方皓 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-06-19 14:38:58 +0800

    公众平台模板头部代码整理

--- templates/platform/library/common_header.lbi.php
+++ templates/platform/library/common_header.lbi.php
@@ -1,175 +1,53 @@
 <?php defined('IN_ECJIA') or exit('No permission resources.');?>
-    <nav class="header-navbar navbar-expand-md navbar navbar-with-menu fixed-top navbar-light navbar-hide-on-scroll navbar-border navbar-shadow navbar-brand-center">
-      <div class="navbar-wrapper">
-        <div class="navbar-header">
-          <ul class="nav navbar-nav flex-row">
-            <li class="nav-item mobile-menu d-md-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="icons-simple-line-icons.html#"><i class="ft-menu font-large-1"></i></a></li>
-            <li class="nav-item"><a class="navbar-brand" href="index.html"><img class="brand-logo" alt="robust admin logo" src="../../../content/apps/platform/statics/platform/images/logo/logo-dark-sm.png">
-                <h3 class="brand-text">ECJia公众平台</h3></a></li>
-            <li class="nav-item d-md-none"><a class="nav-link open-navbar-container" data-toggle="collapse" data-target="#navbar-mobile"><i class="fa fa-ellipsis-v"></i></a></li>
-          </ul>
-        </div>
-        <div class="navbar-container content">
-          <div class="collapse navbar-collapse" id="navbar-mobile">
-            <ul class="nav navbar-nav mr-auto float-left">
-              <li class="nav-item d-none d-md-block"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="icons-simple-line-icons.html#"><i class="ft-menu">         </i></a></li>
-              <li class="dropdown nav-item mega-dropdown"><a class="dropdown-toggle nav-link" href="icons-simple-line-icons.html#" data-toggle="dropdown">Mega</a>
-                <ul class="mega-dropdown-menu dropdown-menu row">
-                  <li class="col-md-2">
-                    <h6 class="dropdown-menu-header text-uppercase mb-1"><i class="fa fa-newspaper-o"></i> News</h6>
-                    <div id="mega-menu-carousel-example">
-                      <div><img class="rounded img-fluid mb-1" src="../../../content/apps/platform/statics/platform/images/slider/slider-2.png" alt="First slide"><a class="news-title mb-0" href="icons-simple-line-icons.html#">Poster Frame PSD</a>
-                        <p class="news-content"><span class="font-small-2">January 26, 2018</span></p>
-                      </div>
-                    </div>
-                  </li>
-                  <li class="col-md-3">
-                    <h6 class="dropdown-menu-header text-uppercase"><i class="fa fa-random"></i> Drill down menu</h6>
-                    <ul class="drilldown-menu">
-                      <li class="menu-list">
-                        <ul>
-                          <li><a class="dropdown-item" href="layout-2-columns.html"><i class="ft-file"></i> Page layouts & Templates</a></li>
-                          <li><a href="icons-simple-line-icons.html#"><i class="ft-align-left"></i> Multi level menu</a>
-                            <ul>
-                              <li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-file-o"></i>  Second level</a></li>
-                              <li><a href="icons-simple-line-icons.html#"><i class="fa fa-star-o"></i> Second level menu</a>
-                                <ul>
-                                  <li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-heart"></i>  Third level</a></li>
-                                  <li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-heart"></i> Third level</a></li>
-                                  <li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-heart"></i> Third level</a></li>
-                                  <li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-heart"></i> Third level</a></li>
-                                </ul>
-                              </li>
-                              <li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-film"></i> Second level, third link</a></li>
-                              <li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-envelope-o"></i> Second level, fourth link</a></li>
-                            </ul>
-                          </li>
-                          <li><a class="dropdown-item" href="color-palette-primary.html"><i class="ft-camera"></i> Color palette system</a></li>
-                          <li><a class="dropdown-item" href="https://pixinvent.com/bootstrap-admin-template/robust/html/ltr/vertical-content-menu-template/sk-2-columns.html"><i class="ft-edit"></i> Page starter kit</a></li>
-                          <li><a class="dropdown-item" href="changelog.html"><i class="ft-minimize-2"></i> Change log</a></li>
-                          <li><a class="dropdown-item" href="https://pixinvent.ticksy.com/"><i class="fa fa-life-ring"></i> Customer support center</a></li>
-                        </ul>
-                      </li>
-                    </ul>
-                  </li>
-
-                </ul>
-              </li>
-              <li class="nav-item d-none d-md-block"><a class="nav-link nav-link-expand" href="icons-simple-line-icons.html#"><i class="ficon ft-maximize"></i></a></li>
-              <li class="nav-item nav-search"><a class="nav-link nav-link-search" href="icons-simple-line-icons.html#"><i class="ficon ft-search"></i></a>
-                <div class="search-input">
-                  <input class="input" type="text" placeholder="Explore Robust...">
-                </div>
-              </li>
-            </ul>
-            <ul class="nav navbar-nav float-right">         
-              <li class="dropdown dropdown-language nav-item">
-              	<a class="dropdown-toggle nav-link" id="dropdown-flag" href="icons-simple-line-icons.html#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="flag-icon flag-icon-cn"></i><span>简体中文</span><span class="selected-language"></span></a>
-                <div class="dropdown-menu" aria-labelledby="dropdown-flag">
-                	<a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="flag-icon flag-icon-cn"></i> 简体中文</a>
-                </div>
-              </li>
-              <li class="dropdown dropdown-notification nav-item"><a class="nav-link nav-link-label" href="icons-simple-line-icons.html#" data-toggle="dropdown"><i class="ficon ft-bell"></i><span class="badge badge-pill badge-default badge-danger badge-default badge-up">5</span></a>
-                <ul class="dropdown-menu dropdown-menu-media dropdown-menu-right">
-                  <li class="dropdown-menu-header">
-                    <h6 class="dropdown-header m-0"><span class="grey darken-2">Notifications</span></h6><span class="notification-tag badge badge-default badge-danger float-right m-0">5 New</span>
-                  </li>
-                  <li class="scrollable-container media-list w-100"><a href="javascript:void(0)">
-                      <div class="media">
-                        <div class="media-left align-self-center"><i class="ft-plus-square icon-bg-circle bg-cyan"></i></div>
-                        <div class="media-body">
-                          <h6 class="media-heading">You have new order!</h6>
-                          <p class="notification-text font-small-3 text-muted">Lorem ipsum dolor sit amet, consectetuer elit.</p><small>
-                            <time class="media-meta text-muted" datetime="2015-06-11T18:29:20+08:00">30 minutes ago</time></small>
+<nav class="header-navbar navbar-expand-md navbar navbar-with-menu fixed-top navbar-light navbar-hide-on-scroll navbar-border navbar-shadow navbar-brand-center">
+	<div class="navbar-wrapper">
+		<div class="navbar-header">
+      		<ul class="nav navbar-nav flex-row">
+        		<li class="nav-item mobile-menu d-md-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="icons-simple-line-icons.html#"><i class="ft-menu font-large-1"></i></a></li>
+        		<li class="nav-item">
+        			<a class="navbar-brand" href="index.html">
+        			<img class="brand-logo" alt="robust admin logo" src="../../../content/apps/platform/statics/platform/images/logo/logo-dark-sm.png">
+            		<h3 class="brand-text">ECJia公众平台</h3>
+            		</a>
+        		</li>
+        		<li class="nav-item d-md-none"><a class="nav-link open-navbar-container" data-toggle="collapse" data-target="#navbar-mobile"><i class="fa fa-ellipsis-v"></i></a></li>
+      		</ul>
+		</div>
+    	<div class="navbar-container content">
+      		<div class="collapse navbar-collapse" id="navbar-mobile">
+        		<ul class="nav navbar-nav mr-auto float-left">
+          			<li class="nav-item d-none d-md-block"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="icons-simple-line-icons.html#"><i class="ft-menu">         </i></a></li>
+                    <!-- {include file="library/common_header_mega.lbi.php"} -->
+          			<li class="nav-item d-none d-md-block"><a class="nav-link nav-link-expand" href="#"><i class="ficon ft-maximize"></i></a></li>
+          			<li class="nav-item nav-search">
+          				<a class="nav-link nav-link-search" href="icons-simple-line-icons.html#"><i class="ficon ft-search"></i></a>
+                        <div class="search-input">
+                          	<input class="input" type="text" placeholder="Explore Robust...">
                         </div>
-                      </div></a><a href="javascript:void(0)">
-                      <div class="media">
-                        <div class="media-left align-self-center"><i class="ft-download-cloud icon-bg-circle bg-red bg-darken-1"></i></div>
-                        <div class="media-body">
-                          <h6 class="media-heading red darken-1">99% Server load</h6>
-                          <p class="notification-text font-small-3 text-muted">Aliquam tincidunt mauris eu risus.</p><small>
-                            <time class="media-meta text-muted" datetime="2015-06-11T18:29:20+08:00">Five hour ago</time></small>
+          			</li>
+        		</ul>
+        		<ul class="nav navbar-nav float-right">         
+          			<li class="dropdown dropdown-language nav-item">
+          				<a class="dropdown-toggle nav-link" id="dropdown-flag" href="icons-simple-line-icons.html#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="flag-icon flag-icon-cn"></i><span>简体中文</span><span class="selected-language"></span></a>
+                        <div class="dropdown-menu" aria-labelledby="dropdown-flag">
+                        	<a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="flag-icon flag-icon-cn"></i> 简体中文</a>
                         </div>
-                      </div></a><a href="javascript:void(0)">
-                      <div class="media">
-                        <div class="media-left align-self-center"><i class="ft-alert-triangle icon-bg-circle bg-yellow bg-darken-3"></i></div>
-                        <div class="media-body">
-                          <h6 class="media-heading yellow darken-3">Warning notifixation</h6>
-                          <p class="notification-text font-small-3 text-muted">Vestibulum auctor dapibus neque.</p><small>
-                            <time class="media-meta text-muted" datetime="2015-06-11T18:29:20+08:00">Today</time></small>
-                        </div>
-                      </div></a><a href="javascript:void(0)">
-                      <div class="media">
-                        <div class="media-left align-self-center"><i class="ft-check-circle icon-bg-circle bg-cyan"></i></div>
-                        <div class="media-body">
-                          <h6 class="media-heading">Complete the task</h6><small>
-                            <time class="media-meta text-muted" datetime="2015-06-11T18:29:20+08:00">Last week</time></small>
-                        </div>
-                      </div></a><a href="javascript:void(0)">
-                      <div class="media">
-                        <div class="media-left align-self-center"><i class="ft-file icon-bg-circle bg-teal"></i></div>
-                        <div class="media-body">
-                          <h6 class="media-heading">Generate monthly report</h6><small>
-                            <time class="media-meta text-muted" datetime="2015-06-11T18:29:20+08:00">Last month</time></small>
-                        </div>
-                      </div></a></li>
-                  <li class="dropdown-menu-footer"><a class="dropdown-item text-muted text-center" href="javascript:void(0)">Read all notifications</a></li>
-                </ul>
-              </li>
-              <li class="dropdown dropdown-notification nav-item"><a class="nav-link nav-link-label" href="icons-simple-line-icons.html#" data-toggle="dropdown"><i class="ficon ft-mail"></i><span class="badge badge-pill badge-default badge-info badge-default badge-up">5              </span></a>
-                <ul class="dropdown-menu dropdown-menu-media dropdown-menu-right">
-                  <li class="dropdown-menu-header">
-                    <h6 class="dropdown-header m-0"><span class="grey darken-2">Messages</span></h6><span class="notification-tag badge badge-default badge-warning float-right m-0">4 New</span>
-                  </li>
-                  <li class="scrollable-container media-list w-100"><a href="javascript:void(0)">
-                      <div class="media">
-                        <div class="media-left"><span class="avatar avatar-sm avatar-online rounded-circle"><img src="../../../content/apps/platform/statics/platform/images/portrait/small/avatar-s-19.png" alt="avatar"><i></i></span></div>
-                        <div class="media-body">
-                          <h6 class="media-heading">Margaret Govan</h6>
-                          <p class="notification-text font-small-3 text-muted">I like your portfolio, let's start.</p><small>
-                            <time class="media-meta text-muted" datetime="2015-06-11T18:29:20+08:00">Today</time></small>
-                        </div>
-                      </div></a><a href="javascript:void(0)">
-                      <div class="media">
-                        <div class="media-left"><span class="avatar avatar-sm avatar-busy rounded-circle"><img src="../../../content/apps/platform/statics/platform/images/portrait/small/avatar-s-2.png" alt="avatar"><i></i></span></div>
-                        <div class="media-body">
-                          <h6 class="media-heading">Bret Lezama</h6>
-                          <p class="notification-text font-small-3 text-muted">I have seen your work, there is</p><small>
-                            <time class="media-meta text-muted" datetime="2015-06-11T18:29:20+08:00">Tuesday</time></small>
-                        </div>
-                      </div></a><a href="javascript:void(0)">
-                      <div class="media">
-                        <div class="media-left"><span class="avatar avatar-sm avatar-online rounded-circle"><img src="../../../content/apps/platform/statics/platform/images/portrait/small/avatar-s-3.png" alt="avatar"><i></i></span></div>
-                        <div class="media-body">
-                          <h6 class="media-heading">Carie Berra</h6>
-                          <p class="notification-text font-small-3 text-muted">Can we have call in this week ?</p><small>
-                            <time class="media-meta text-muted" datetime="2015-06-11T18:29:20+08:00">Friday</time></small>
-                        </div>
-                      </div></a><a href="javascript:void(0)">
-                      <div class="media">
-                        <div class="media-left"><span class="avatar avatar-sm avatar-away rounded-circle"><img src="../../../content/apps/platform/statics/platform/images/portrait/small/avatar-s-6.png" alt="avatar"><i></i></span></div>
-                        <div class="media-body">
-                          <h6 class="media-heading">Eric Alsobrook</h6>
-                          <p class="notification-text font-small-3 text-muted">We have project party this saturday.</p><small>
-                            <time class="media-meta text-muted" datetime="2015-06-11T18:29:20+08:00">last month</time></small>
-                        </div>
-                      </div></a></li>
-                  <li class="dropdown-menu-footer"><a class="dropdown-item text-muted text-center" href="javascript:void(0)">Read all messages</a></li>
-                </ul>
-              </li>
-              <li class="dropdown dropdown-user nav-item">
-              	<a class="dropdown-toggle nav-link dropdown-user-link" href="{$current_user->getProfileSettingUrl()}#" data-toggle="dropdown">
-              		<span class="avatar avatar-online"><img src="{$current_user->getAvatarUrl()}" alt="{{$current_user->getUserName()}}"><i></i></span>
-              		<span class="user-name">{$current_user->getUserName()}</span>
-              	</a>
-                <div class="dropdown-menu dropdown-menu-right">
-                	<a class="dropdown-item" href="{$current_user->getProfileSettingUrl()}"><i class="ft-user"></i> 个人设置</a>
-                  	<div class="dropdown-divider"></div>
-                  	<a class="dropdown-item" href="{url path='platform/privilege/logout'}"><i class="ft-power"></i> 退出</a>
-            	</div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </nav>
+          			</li>
+                    <!-- {include file="library/common_header_notifications.lbi.php"} -->
+                    <!-- {include file="library/common_header_messages.lbi.php"} -->
+          			<li class="dropdown dropdown-user nav-item">
+          				<a class="dropdown-toggle nav-link dropdown-user-link" href="{$current_user->getProfileSettingUrl()}#" data-toggle="dropdown">
+          					<span class="avatar avatar-online"><img src="{$current_user->getAvatarUrl()}" alt="{{$current_user->getUserName()}}"><i></i></span>
+          					<span class="user-name">{$current_user->getUserName()}</span>
+          				</a>
+                        <div class="dropdown-menu dropdown-menu-right">
+                        	<a class="dropdown-item" href="{$current_user->getProfileSettingUrl()}"><i class="ft-user"></i> 个人设置</a>
+                          	<div class="dropdown-divider"></div>
+                          	<a class="dropdown-item" href="{url path='platform/privilege/logout'}"><i class="ft-power"></i> 退出</a>
+                    	</div>
+          			</li>
+        		</ul>
+      		</div>
+    	</div>
+	</div>
+</nav>
---
+++ templates/platform/library/common_header_mega.lbi.php
@@ -0,0 +1,47 @@
+<?php defined('IN_ECJIA') or exit('No permission resources.');?>
+<li class="dropdown nav-item mega-dropdown">
+<a class="dropdown-toggle nav-link" href="icons-simple-line-icons.html#" data-toggle="dropdown">Mega</a>
+    <ul class="mega-dropdown-menu dropdown-menu row">
+		<li class="col-md-2">
+        	<h6 class="dropdown-menu-header text-uppercase mb-1"><i class="fa fa-newspaper-o"></i> News</h6>
+        	<div id="mega-menu-carousel-example">
+              	<div>
+              		<img class="rounded img-fluid mb-1" src="../../../content/apps/platform/statics/platform/images/slider/slider-2.png" alt="First slide">
+              		<a class="news-title mb-0" href="icons-simple-line-icons.html#">Poster Frame PSD</a>
+                    <p class="news-content"><span class="font-small-2">January 26, 2018</span></p>
+              	</div>
+            </div>
+      	</li>
+      	<li class="col-md-3">
+            <h6 class="dropdown-menu-header text-uppercase"><i class="fa fa-random"></i> Drill down menu</h6>
+            <ul class="drilldown-menu">
+              	<li class="menu-list">
+                	<ul>
+                      	<li><a class="dropdown-item" href="layout-2-columns.html"><i class="ft-file"></i> Page layouts & Templates</a></li>
+                      	<li><a href="icons-simple-line-icons.html#"><i class="ft-align-left"></i> Multi level menu</a>
+                            <ul>
+                              	<li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-file-o"></i>  Second level</a></li>
+                              	<li>
+                              		<a href="icons-simple-line-icons.html#"><i class="fa fa-star-o"></i> Second level menu</a>
+                            		<ul>
+                              			<li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-heart"></i>  Third level</a></li>
+                                  		<li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-heart"></i> Third level</a></li>
+                                  		<li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-heart"></i> Third level</a></li>
+                                  		<li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-heart"></i> Third level</a></li>
+                                	</ul>
+                              	</li>
+                              	<li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-film"></i> Second level, third link</a></li>
+                              	<li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-envelope-o"></i> Second level, fourth link</a></li>
+                        	</ul>
+                      	</li>
+                      	<li><a class="dropdown-item" href="color-palette-primary.html"><i class="ft-camera"></i> Color palette system</a></li>
+                      	<li><a class="dropdown-item" href="https://pixinvent.com/bootstrap-admin-template/robust/html/ltr/vertical-content-menu-template/sk-2-columns.html"><i class="ft-edit"></i> Page starter kit</a></li>
+                      	<li><a class="dropdown-item" href="changelog.html"><i class="ft-minimize-2"></i> Change log</a></li>
+						<li><a class="dropdown-item" href="https://pixinvent.ticksy.com/"><i class="fa fa-life-ring"></i> Customer support center</a></li>
+					</ul>
+				</li>
+			</ul>
+		</li>
+
+	</ul>
+</li>
