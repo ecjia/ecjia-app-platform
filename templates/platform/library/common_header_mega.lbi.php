fe968222f5ccc9221ffd1efaf0be6420c27fd8f5
东方皓 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-06-20 11:54:29 +0800

    优化顶部浮窗效果

--- templates/platform/library/common_header_mega.lbi.php
+++ templates/platform/library/common_header_mega.lbi.php
@@ -2,7 +2,7 @@
 <li class="dropdown nav-item mega-dropdown">
 <a class="dropdown-toggle nav-link" href="icons-simple-line-icons.html#" data-toggle="dropdown">公众平台</a>
     <ul class="mega-dropdown-menu dropdown-menu row">
-		<li class="col-md-2">
+		<li class="col-md-12">
         	<h6 class="dropdown-menu-header text-uppercase mb-1"><i class="fa fa-newspaper-o"></i> 公众平台</h6>
         	<div id="mega-menu-carousel-example">
               	<div>
