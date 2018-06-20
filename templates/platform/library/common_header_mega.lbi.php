2cc7584d4dddab995c10c313a64d54ec1b2a58eb
东方皓 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-06-20 10:02:39 +0800

    修改顶部浮窗

--- templates/platform/library/common_header_mega.lbi.php
+++ templates/platform/library/common_header_mega.lbi.php
@@ -11,36 +11,7 @@
               	</div>
             </div>
       	</li>
-      	<li class="col-md-3">
-            <h6 class="dropdown-menu-header text-uppercase"><i class="fa fa-random"></i> Drill down menu</h6>
-            <ul class="drilldown-menu">
-              	<li class="menu-list">
-                	<ul>
-                      	<li><a class="dropdown-item" href="layout-2-columns.html"><i class="ft-file"></i> Page layouts & Templates</a></li>
-                      	<li><a href="icons-simple-line-icons.html#"><i class="ft-align-left"></i> Multi level menu</a>
-                            <ul>
-                              	<li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-file-o"></i>  Second level</a></li>
-                              	<li>
-                              		<a href="icons-simple-line-icons.html#"><i class="fa fa-star-o"></i> Second level menu</a>
-                            		<ul>
-                              			<li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-heart"></i>  Third level</a></li>
-                                  		<li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-heart"></i> Third level</a></li>
-                                  		<li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-heart"></i> Third level</a></li>
-                                  		<li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-heart"></i> Third level</a></li>
-                                	</ul>
-                              	</li>
-                              	<li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-film"></i> Second level, third link</a></li>
-                              	<li><a class="dropdown-item" href="icons-simple-line-icons.html#"><i class="fa fa-envelope-o"></i> Second level, fourth link</a></li>
-                        	</ul>
-                      	</li>
-                      	<li><a class="dropdown-item" href="color-palette-primary.html"><i class="ft-camera"></i> Color palette system</a></li>
-                      	<li><a class="dropdown-item" href="https://pixinvent.com/bootstrap-admin-template/robust/html/ltr/vertical-content-menu-template/sk-2-columns.html"><i class="ft-edit"></i> Page starter kit</a></li>
-                      	<li><a class="dropdown-item" href="changelog.html"><i class="ft-minimize-2"></i> Change log</a></li>
-						<li><a class="dropdown-item" href="https://pixinvent.ticksy.com/"><i class="fa fa-life-ring"></i> Customer support center</a></li>
-					</ul>
-				</li>
-			</ul>
-		</li>
+      	
 
 	</ul>
 </li>
