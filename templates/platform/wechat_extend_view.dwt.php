aed011805821dfc810fe81f65946c063584f5662
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-06-26 17:58:49 +0800

    提交公众平台扩展修改

--- templates/platform/wechat_extend_view.dwt.php
+++ templates/platform/wechat_extend_view.dwt.php
@@ -20,18 +20,24 @@
                 </h4>
             </div>
             <div class="card-body">
+            
 				<div class="highlight_box global icon_wrap group" id="js_apply_btn">
-					{if !$bd.ext_config}<a class="btn btn-success btn-min-width f_r js_apply" href="javascript:;">开通</a>{/if}
+					{if !$bd.ext_config}
+						<a class="btn btn-success btn-min-width f_r extend_handle" href="{RC_Uri::url('platform/platform_extend/wechat_extend_insert')}" data-code="{$info.ext_code}" data-config="{$info.ext_config}">开通</a>
+					{else}
+						<a class="btn btn-danger btn-min-width f_r extend_handle" href="{RC_Uri::url('platform/platform_extend/wechat_extend_remove')}" data-code="{$info.ext_code}">关闭</a>
+					{/if}
 					<div class="fonticon-container">
 						<div class="fonticon-wrap">
 							<img class="icon-extend" src="{$images_url}extend.png" />
 						</div>
 					</div>
-					<h4 class="title">{$bd.ext_name}</h4>
+					<h4 class="title">{$info.ext_name}</h4>
 					<p class="desc" id="js_status">
 					{if !$bd.ext_config}<span>未开通</span>{else}该功能已通过申请，可正常使用{/if}
 					</p>
 				</div>
+				
 				<div class="carkticket_index">
 					<div class="intro">
 						<dl class="">
@@ -42,6 +48,34 @@
 						</dl>
 					</div>
 				</div>
+				
+				{if $bd}
+				<div class="carkticket_index m_t50">
+					<div class="intro">
+						<dl class="">
+							<dt><span class="ico_intro ico ico_1 l"></span>
+								<h4 class="card-title">插件配置</h4>
+							</dt>
+						</dl>
+					</div>
+					<form class="form" method="post" name="theForm" action="{$form_action}">
+						<div class="card-body">
+							<div class="form-body">
+								<div class="form-group row">
+									<label class="col-lg-2 label-control text-right">扩展名称：</label>
+									<div class="col-lg-8 controls">
+										{$info.ext_name}
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="modal-footer justify-content-center">
+							<input type="hidden" name="ext_code" value="{$bd.ext_code}" />
+							<input type="submit" class="btn btn-outline-primary" value="更新"/>
+						</div>
+					</form>
+				</div>
+				{/if}
             </div>
         </div>
     </div>
