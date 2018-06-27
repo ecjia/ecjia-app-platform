f32acba1439c7a92a290165e3c3df341e15118a0
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-06-27 11:01:33 +0800

    修改插件配置页面

--- templates/platform/wechat_extend_view.dwt.php
+++ templates/platform/wechat_extend_view.dwt.php
@@ -20,7 +20,6 @@
                 </h4>
             </div>
             <div class="card-body">
-            
 				<div class="highlight_box global icon_wrap group" id="js_apply_btn">
 					{if !$bd}
 						<a class="btn btn-success btn-min-width f_r extend_handle" href="{RC_Uri::url('platform/platform_extend/wechat_extend_insert')}" data-code="{$info.ext_code}">开通</a>
@@ -37,7 +36,6 @@
 					{if !$bd}<span>未开通</span>{else}该功能已通过申请，可正常使用{/if}
 					</p>
 				</div>
-				
 				<div class="carkticket_index">
 					<div class="intro">
 						<dl class="">
@@ -48,61 +46,68 @@
 						</dl>
 					</div>
 				</div>
-				
-				{if $bd}
-				<div class="carkticket_index m_t50">
-					<div class="intro">
-						<dl class="">
-							<dt><span class="ico_intro ico ico_1 l"></span>
-								<h4 class="card-title">插件配置</h4>
-							</dt>
-						</dl>
-					</div>
-					<form class="form" method="post" name="theForm" action="{$form_action}">
-						<div class="card-body">
-							<div class="form-body">
-								<div class="form-group row">
-									<label class="col-lg-2 label-control text-right">扩展名称：</label>
-									<div class="col-lg-8 controls">
-										{$info.ext_name}
-									</div>
-								</div>
-								
-								<!-- {foreach from=$bd.ext_config item=config key=key} -->
-								<div class="form-group row">
-									<label class="col-lg-2 label-control text-right">{$config.label}</label>
-									<div class="col-lg-6 controls">
-										<!-- {if $config.type == "text"} -->
-										<input class="form-control" id="cfg_value[]" name="cfg_value[]" type="{$config.type}" value="{$config.value}" size="40" />
-										<!-- {elseif $config.type == "textarea"} -->
-										<textarea class="form-control" id="cfg_value[]" name="cfg_value[]" cols="80" rows="5">{$config.value}</textarea>
-										<!-- {elseif $config.type == "select"} -->
-										<select class="form-control" id="cfg_value[]" name="cfg_value[]"  >
-											<!-- {html_options options=$config.range selected=$config.value} -->
-										</select>
-										<!-- {/if} -->
-										<input name="cfg_name[]" type="hidden" value="{$config.name}" />
-										<input name="cfg_type[]" type="hidden" value="{$config.type}" />
-										<input name="cfg_lang[]" type="hidden" value="{$config.lang}" />
-										{if $config.desc}
-			    						<div class="help-block">{$config.desc}</div>
-			    						{/if}
-										<!--the tenpay code -->
-									</div>
-								</div>
-								<!-- {/foreach} -->
-				
-							</div>
-						</div>
-						<div class="modal-footer justify-content-center">
-							<input type="hidden" name="ext_code" value="{$bd.ext_code}" />
-							<input type="submit" class="btn btn-outline-primary" value="更新"/>
-						</div>
-					</form>
-				</div>
-				{/if}
             </div>
         </div>
     </div>
 </div>
+
+{if $bd}
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+       		<div class="card-header">
+                <h4 class="card-title">
+                	插件配置
+                </h4>
+            </div>
+        	<div class="card-body">
+				<form class="form" method="post" name="theForm" action="{$form_action}">
+					<div class="card-body">
+						<div class="form-body">
+							<div class="form-group row">
+								<label class="col-lg-2 label-control text-right">扩展名称：</label>
+								<div class="col-lg-8 controls">
+									{$info.ext_name}
+								</div>
+							</div>
+							<!-- {foreach from=$bd.ext_config item=config key=key} -->
+							<div class="form-group row">
+								<label class="col-lg-2 label-control text-right">{$config.label}</label>
+								<div class="col-lg-6 controls">
+									<!-- {if $config.type == "text"} -->
+									<input class="form-control" id="cfg_value[]" name="cfg_value[]" type="{$config.type}" value="{$config.value}" size="40" />
+									<!-- {elseif $config.type == "textarea"} -->
+									<textarea class="form-control" id="cfg_value[]" name="cfg_value[]" cols="80" rows="5">{$config.value}</textarea>
+									<!-- {elseif $config.type == "select"} -->
+									<select class="form-control" id="cfg_value[]" name="cfg_value[]"  >
+										<!-- {html_options options=$config.range selected=$config.value} -->
+									</select>
+									<!-- {elseif $config.type == "radiobox"} -->
+									<!-- {foreach from=$config.range item=val key=k} -->
+										<input id="radio_{$k}" type="radio" name="cfg_value[]" value="{$k}" {if $config.value eq $k} checked="true" {/if}/><label for="radio_{$k}">{$val}</label>
+									<!-- {/foreach} -->
+									<!-- {/if} -->
+									<input name="cfg_name[]" type="hidden" value="{$config.name}" />
+									<input name="cfg_type[]" type="hidden" value="{$config.type}" />
+									<input name="cfg_lang[]" type="hidden" value="{$config.lang}" />
+									{if $config.desc}
+		    						<div class="help-block">{$config.desc}</div>
+		    						{/if}
+									<!--the tenpay code -->
+								</div>
+							</div>
+							<!-- {/foreach} -->
+						</div>
+					</div>
+					<div class="modal-footer justify-content-center">
+						<input type="hidden" name="ext_code" value="{$bd.ext_code}" />
+						<input type="submit" class="btn btn-outline-primary" value="更新"/>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>	
+{/if}
+				
 <!-- {/block} -->
