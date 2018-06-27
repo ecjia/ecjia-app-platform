cb76384ac1a14f8f3625943f13271b74105c15d4
东方皓 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-06-27 17:15:51 +0800

    增加公众平台插件的icon url地址获取

--- classes/Plugin/PlatformAbstract.php
+++ classes/Plugin/PlatformAbstract.php
@@ -8,6 +8,11 @@ abstract class PlatformAbstract extends AbstractPlugin
 {
     
     /**
+     * 获取iconUrl
+     */
+    abstract public function getPluginIconUrl();
+    
+    /**
      * 插件返回数据统一接口
      */
     abstract public function event_reply();
