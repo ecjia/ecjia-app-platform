ce4adff35f6f87c9260d0684da4dafd2ff410faf
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-06-28 13:26:06 +0800

    提交底部样式修改

--- templates/platform/library/common_footer.lbi.php
+++ templates/platform/library/common_footer.lbi.php
@@ -1,10 +1,7 @@
 <?php defined('IN_ECJIA') or exit('No permission resources.');?> 
 <footer class="footer footer-static footer-transparent">
 	<p class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2">
-		<span class="float-md-left d-block d-md-inline-block">
-		Copyright &copy; 2018 {ecjia::config('shop_name')} {if ecjia::config('icp_number', 2)}<a href="http://www.miibeian.gov.cn" target="_blank">{ecjia::config('icp_number')}</a>{/if}
-		</span>
-		<span class="float-md-right d-block d-md-inline-blockd-none d-lg-block">当前正在访问【{$currentStore->getStoreName()}】的{$platformAccount->getAccountName()} <i class="ft-eye pink"></i></span>
+		<span class="d-block d-md-inline-blockd-none d-lg-block">Copyright &copy; 2018 {ecjia::config('shop_name')} {if ecjia::config('icp_number', 2)}<a href="http://www.miibeian.gov.cn" target="_blank">{ecjia::config('icp_number')}</a>{/if}</span>
 	</p>
 </footer>
 
