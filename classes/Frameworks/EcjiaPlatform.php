2a4d1cf2c7e21e6ce3b846279761bc8b7ec94726
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-07-16 17:24:59 +0800

    控制器基类访问权限判断，直接中断程序

--- classes/Frameworks/EcjiaPlatform.php
+++ classes/Frameworks/EcjiaPlatform.php
@@ -73,7 +73,6 @@ use RC_Plugin;
 use RC_Uri;
 use RC_File;
 use RC_Ip;
-use RC_Response;
 use Smarty;
 
 use Ecjia\App\Platform\Frameworks\Component\Screen;
@@ -170,15 +169,15 @@ abstract class EcjiaPlatform extends ecjia_base implements ecjia_template_filelo
 		    if (is_pjax()) {
 		        Screen::$current_screen->add_nav_here(new admin_nav_here(__('系统提示')));
 		        $this->showmessage(RC_Lang::get('system::system.priv_error'), ecjia::MSGTYPE_HTML | ecjia::MSGSTAT_ERROR, array('links' => array(array('text' => __('重新登录'), 'href' => RC_Uri::url('staff/privilege/login')))));
-                RC_Response::send();
+                royalcms('response')->send();
 		        exit();
 		    } elseif (is_ajax()) {
 		        $this->showmessage(RC_Lang::get('system::system.priv_error'), ecjia::MSGTYPE_JSON | ecjia::MSGSTAT_ERROR);
-                RC_Response::send();
+                royalcms('response')->send();
 		        exit();
 		    } else {
 		        $this->redirect(RC_Uri::url('@privilege/login'));
-                RC_Response::send();
+                royalcms('response')->send();
 		        exit();
 		    }
 		}
