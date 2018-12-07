da0ee74d3db088caba6a394b6531022d09851738
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-12-07 10:14:56 +0800

    更新公众平台基类控制器

--- classes/Frameworks/EcjiaPlatform.php
+++ classes/Frameworks/EcjiaPlatform.php
@@ -88,8 +88,6 @@ define('IN_PLATFORM', true);
 
 abstract class EcjiaPlatform extends ecjia_base implements ecjia_template_fileloader {
 
-	private $public_route;
-	
 	/**
 	 * 
 	 * @var \Ecjia\App\Platform\Frameworks\Platform\Account $platformAccount
